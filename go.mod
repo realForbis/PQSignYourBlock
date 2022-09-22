d69d1104d974b7175271dead409079e73b30e8c9
Lukáš Heczko <37193612+realForbis@users.noreply.github.com>
2022-09-22 21:20:11 +0200

    add sphincs

--- go.mod
+++ go.mod
@@ -4,4 +4,8 @@ go 1.19
 
 require golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90
 
-require golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+require (
+	github.com/dis2/go-dilithium v0.0.0-20180307155033-ff250ffe14a8 // indirect
+	github.com/kasperdi/SPHINCSPLUS-golang v0.0.0-20220523105241-2e1d517cf3b3 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+)
--- src/main.go
+++ src/main.go
@@ -3,13 +3,28 @@ package main
 import (
 	"fmt"
 
-	dlth "github.com/realForbis/PQSignYourBlock/src/dilithium"
+	"github.com/dis2/go-dilithium/dilithium"
+	"github.com/kasperdi/SPHINCSPLUS-golang/sphincs"
 )
 
-func generateDilithiumKeyPair(seed []byte) {
-	PubKey, PrivKey, _ := dlth.KeyPair(nil)
-	fmt.Println(PubKey.Bytes(), PrivKey.Bytes())
+type Account struct {
+	pubKey  [1472]byte
+	privKey [3504]byte
+}
+
+func generateDilithiumKeyPair(seed []byte) *Account {
+	_PubKey, _PrivKey, _ := dilithium.KeyPair(nil)
+	return &Account{
+		pubKey:  *_PubKey.Bytes(),
+		privKey: *_PrivKey.Bytes(),
+	}
+}
+
+func generateSphncsKeyPair() {
+	_PrivKey, _PubKey := sphincs.Spx_keygen(nil)
+	fmt.Printf("public key: %x\n", _PubKey)
+	fmt.Printf("private key: %x\n", _PrivKey)
 }
 func main() {
-
+	generateSphncsKeyPair()
 }
